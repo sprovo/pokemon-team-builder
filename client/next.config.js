07786cd590d3caedd724ed08e1c3835c481208cc
Sprovo <sandricoprovo@redspace.com>
2023-01-26 16:30:15 -0400

    feat: Adds tailwinds to project.

--- client/next.config.js
+++ client/next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
+    experimental: {
+        appDir: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'raw.githubusercontent.com',
+                port: '',
+                pathname: '/PokeAPI/sprites/master/sprites/pokemon/**',
+            },
+        ],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,14 +1,15 @@
-import './globals.css'
+import './reset.css';
+import './globals.css';
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      {/*
+    return (
+        <html lang="en">
+            {/*
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+            <head />
+            <body className="bg-slate-900">{children}</body>
+        </html>
+    );
 }
